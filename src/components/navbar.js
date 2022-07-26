18171e05cf5797d92258f85387f1bb8b9b9497a0
jason-fine <jasonfine94@gmail.com>
2022-07-26 18:09:36 -0400

    Working on navbar component, brought in seed data

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavBarContainer = styled.div`
+    background-color : yellow;
+    width : 100%;
+    text-decoration : underline;
+
+
+`
+
+const FoodCategories = ({foodCategories}) => {
+    return (
+        <div>
+            {foodCategories.map (foodCategory => {
+                return (
+                    <NavBarContainer>
+                        <h1>{foodCategory.name}</h1>
+                    </NavBarContainer>
+                )
+            })}
+        </div>
+    );
+}
+
+export default FoodCategories
