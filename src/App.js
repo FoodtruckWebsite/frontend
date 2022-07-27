9060e608ac4602f8b938b95481c73a4b576968fe
joysimon <joysimon1976@gmail.com>
2022-07-27 18:17:09 -0500

    Resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
+import {useState, useEffect} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+
 
 function App() {
-  const [ trucks, setTrucks ] = useState([])
-
+  const [trucks, setTrucks] = useState([])
   useEffect(() => {
-    fetch('http://localhost:3000/NOW')
-    .then( res => res.json())
-    .then( trucks => setTrucks(trucks))
+    fetch('http://localhost:8000/truck')
+    .then(res => res.json)
+    .then(items => setTrucks(items))
   }, [])
-
   console.log(trucks)
-
   return (
     <div>
       <Routes>
-        <Route path='/' element={ <Home trucks={trucks}/> } />
+          <Route path='/' element={ <Home trucks = {trucks}/>}/>
       </Routes>
     </div>
   );
