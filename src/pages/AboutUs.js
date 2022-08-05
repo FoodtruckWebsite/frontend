5800f00cb4e562453112bdf7115f84d9f33e0073
jason-fine <74693076+jason-fine@users.noreply.github.com>
2022-08-04 21:39:06 -0400

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,9 +9,9 @@ const AboutUs = ({  }) => {
     return (
       <StyledForm>
         <h1>About Us</h1>
-        <h2>Osvaldo Gonzalez: https://github.com/ogonz801</h2>
-        <h2>Joy Simon: https://github.com/j4simon</h2>
-        <h2>Jason Fine: https://github.com/jason-fine </h2>
+        <a href="https://github.com/ogonz801"><h2>Osvaldo Gonzalez</h2></a>
+        <a href="https://github.com/j4simon"><h2>Joy Simon</h2></a>
+        <a href="https://github.com/jason-fine"><h2>Jason Fine</h2></a>
       </StyledForm>
           
     
